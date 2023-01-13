1b4429bc47961591361bc62a4e64b7ebad9de3ba
alxmart <alxmartinez@gmail.com>
2023-01-13 17:20:47 -0300

    Added textviews , code for onClick, layout adjusts.

--- app/src/main/java/com/androidatc/lab_layouts_styles_themes/MainActivity.kt
+++ app/src/main/java/com/androidatc/lab_layouts_styles_themes/MainActivity.kt
@@ -2,10 +2,22 @@ package com.androidatc.lab_layouts_styles_themes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun display(view: View){
+        info.text="Android Application Development ,Android Security Essentials and Monetize Android Applications"
+    }
+
+
+
+
+
 }
