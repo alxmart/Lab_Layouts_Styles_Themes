93eed84d4a2ec4f99150fb277efe0070cb8b6442
alxmart <alxmartinez@gmail.com>
2023-01-13 16:12:08 -0300

    Project created on Android Studio.

---
+++ app/src/main/java/com/androidatc/lab_layouts_styles_themes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androidatc.lab_layouts_styles_themes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
